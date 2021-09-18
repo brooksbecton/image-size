0e98dfaad9aaaef8d696d68c02eab63135f84919
Brooks Becton <bbecton@babelstreet.com>
2021-09-18 09:50:31 -0400

    Add cors package

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,30 +1,31 @@
 import * as functions from "firebase-functions";
 import * as https from "https";
+import * as cors from "cors";
 import sizeOf from "image-size";
 import {isValidHttpUrl} from "./utils/isValidHttpUrl";
 
 
 export const imageSize = functions.https.onRequest((request, response) => {
-  const rawUrl = request.query.url as string;
-  response.set("Access-Control-Allow-Origin", "*");
+  cors()(request, response, () => {
+    const rawUrl = request.query.url as string;
 
+    if (!isValidHttpUrl(rawUrl)) {
+      response.send("Bad URL");
+      return;
+    }
+    const {href: url} = new URL(rawUrl);
+    functions.logger.info(`URL: ${url}`, {structuredData: true});
 
-  if (!isValidHttpUrl(rawUrl)) {
-    response.send("Bad URL");
-    return;
-  }
-  const {href: url} = new URL(rawUrl);
-  functions.logger.info(`URL: ${url}`, {structuredData: true});
-
-  https.get(url, function(r) {
-    const chunks: Uint8Array[] = [];
-    r
-        .on("data", function(chunk) {
-          chunks.push(chunk);
-        })
-        .on("end", function() {
-          const buffer = Buffer.concat(chunks);
-          response.send(sizeOf(buffer));
-        });
+    https.get(url, function(r) {
+      const chunks: Uint8Array[] = [];
+      r
+          .on("data", function(chunk) {
+            chunks.push(chunk);
+          })
+          .on("end", function() {
+            const buffer = Buffer.concat(chunks);
+            response.send(sizeOf(buffer));
+          });
+    });
   });
 });
