be7e29517e3fb9db99d554ede340c8f7b0df6524
Brooks Becton <bbecton@babelstreet.com>
2021-09-19 08:45:28 -0400

    Only allow client url in production

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,28 +4,33 @@ import * as cors from "cors";
 import sizeOf from "image-size";
 import {isValidHttpUrl} from "./utils/isValidHttpUrl";
 
+const isProduction = functions.config().runtime?.env === "production";
+
+const corsConfig = {
+  origin: isProduction ?
+    "https://silent-cartographer-client-mmm8q.ondigitalocean.app" :
+    undefined,
+};
 
 export const imageSize = functions.https.onRequest((request, response) => {
-  cors()(request, response, () => {
+  cors(corsConfig)(request, response, () => {
     const rawUrl = request.query.url as string;
-
     if (!isValidHttpUrl(rawUrl)) {
       response.send("Bad URL");
       return;
     }
+
     const {href: url} = new URL(rawUrl);
     functions.logger.info(`URL: ${url}`, {structuredData: true});
 
     https.get(url, function(r) {
       const chunks: Uint8Array[] = [];
-      r
-          .on("data", function(chunk) {
-            chunks.push(chunk);
-          })
-          .on("end", function() {
-            const buffer = Buffer.concat(chunks);
-            response.send(sizeOf(buffer));
-          });
+      r.on("data", function(chunk) {
+        chunks.push(chunk);
+      }).on("end", function() {
+        const buffer = Buffer.concat(chunks);
+        response.send(sizeOf(buffer));
+      });
     });
   });
 });
